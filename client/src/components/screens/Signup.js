0647276d1596bc0fc43c4113d5c14674ab900512
Shubhankar Naik <46858142+shubhankar-naik@users.noreply.github.com>
2020-06-28 14:27:11 +0530

    Update Signup.js

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -91,15 +91,15 @@ const SignIn  = ()=>{
                 value={password}
                 onChange={(e)=>setPasword(e.target.value)}
                 />
-                <div className="file-field input-field">
-                <div className="btn #64b5f6 blue darken-1">
-                    <span>Upload pic</span>
-                    <input type="file" onChange={(e)=>setImage(e.target.files[0])} />
-                </div>
-                <div className="file-path-wrapper">
-                    <input className="file-path validate" type="text" />
-                </div>
-                </div>
+//                 <div className="file-field input-field">
+//                 <div className="btn #64b5f6 blue darken-1">
+//                     <span>Upload pic</span>
+//                     <input type="file" onChange={(e)=>setImage(e.target.files[0])} />
+//                 </div>
+//                 <div className="file-path-wrapper">
+//                     <input className="file-path validate" type="text" />
+//                 </div>
+//                 </div>
                 <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
                 onClick={()=>PostData()}
                 >
